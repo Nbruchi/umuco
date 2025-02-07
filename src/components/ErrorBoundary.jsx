74dd633930d44275bbdba49c136ec7ed501e8435
Nbruchi <nkundabagenzibruce@gmail.com>
2025-02-07 17:20:14 +0200

    umuco

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,33 @@
+// src/components/ErrorBoundary.jsx
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        console.error("Error caught in ErrorBoundary:", error);
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.error("Uncaught error:", error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div>
+                    <h1>Something went wrong.</h1>
+                    <p>Please refresh the page or contact support.</p>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+import { Html, useProgress } from "@react-three/drei";
+
+const Loader = () => {
+    const { progress } = useProgress();
+
+    return (
+        <Html center>
+            <div className="flex items-center justify-center relative">
+                <div className="w-40 h-40 border-4 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+                <div className="text-blue-500 text-2xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    {Math.round(progress)}%
+                </div>
+            </div>
+        </Html>
+    );
+};
+
+export default Loader;
